118593b671ef86283bccd1def412e6ea159402a1
Zyad Elbehiry <zyadelbehiry@gmail.com>
2025-09-24 19:53:44 +0300

    fixing-cart-and-wishlist

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -86,6 +86,7 @@ import { ProductCardProps } from "../types/types";
 
 // Fix the base URL handling
 export function getBaseUrl() {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 }
